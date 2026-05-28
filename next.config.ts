12cbf2593318a096cbd16d6d0b662b7d5b2a2ae1
Dana Romero <cata0614@gmail.com>
2026-05-28 09:56:55 -0500

    volver

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 👇 Para GitHub Pages (cambia 'nombre-repo' por el tuyo)
-  basePath: "/nexoweb",
-  assetPrefix: "/nexoweb",
-
-  // 👇 Modo estático
   output: "export",
-
-  // 👇 Deshabilita imágenes optimizadas (no funcionan en estático)
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
