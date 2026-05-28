eb18063b812e726ffd310cf9af70a55549c1e3c4
Dana Romero <cata0614@gmail.com>
2026-05-28 09:34:15 -0500

    estatico para ver en git

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 👇 Para GitHub Pages (cambia 'nombre-repo' por el tuyo)
+  basePath: "/nombre-de-tu-repositorio",
+  assetPrefix: "/nombre-de-tu-repositorio",
+
+  // 👇 Modo estático
   output: "export",
-  //* basePath: "/Nexoweb", //
-  //* assetPrefix: "/Nexoweb/",
+
+  // 👇 Deshabilita imágenes optimizadas (no funcionan en estático)
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
